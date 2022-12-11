33e2175e9d281808d927861d3f04e379fc1d1727
mevshnu <vishnusivan1000@gmail.com>
2022-12-11 12:48:28 +0530

    pl

--- src/main/java/com/example/Nest/controller/EmployeeController.java
+++ src/main/java/com/example/Nest/controller/EmployeeController.java
@@ -1,4 +1,30 @@
 package com.example.Nest.controller;
 
+import com.example.Nest.dao.EmployeeDao;
+import com.example.Nest.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+@RestController
 public class EmployeeController {
+    @Autowired
+    private EmployeeDao empdao;
+
+    @PostMapping(path="/userlogin",consumes = "application/json",produces = "application/json")
+    public HashMap <String,String> UserLogin(@RequestBody Employee emp)
+    {
+        List<Employee> employees =(List<Employee>) empdao.GetUserLogin(emp.getUsername(), emp.getPassword());
+        HashMap<String,String> hashMap = new HashMap<>();
+        if(employees.size() ==0 ){
+            hashMap.put("status","failed");
+        }else{
+            hashMap.put("status","success");
+        }
+        return hashMap;
+    }
 }
--- src/main/java/com/example/Nest/dao/EmployeeDao.java
+++ src/main/java/com/example/Nest/dao/EmployeeDao.java
@@ -1,4 +1,16 @@
 package com.example.Nest.dao;
 
-public interface EmployeeDao {
+import com.example.Nest.model.Employee;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface EmployeeDao extends CrudRepository <Employee,Integer>
+
+{
+
+    @Query(value = "SELECT `id`, `emp_code`, `emp_designation`, `emp_email`, `emp_name`, `emp_phone`, `password`, `username` FROM `employee` WHERE `username` = :username AND `password` = :password", nativeQuery = true)
+    List<Employee> GetUserLogin(@Param("username") String username, @Param("password") String password);
 }
